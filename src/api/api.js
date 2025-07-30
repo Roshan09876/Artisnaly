6be153f945151c1cce103d08d2a8f16a7435ef72
Roshan Kumar Khadka <khadkaroshan715@gmail.com>
2025-07-30 15:28:27 +0545

    update/delete

--- src/api/api.js
+++ src/api/api.js
@@ -10,7 +10,7 @@ const Api = axios.create({
 })
 
 const ApiWithFormData = axios.create({
-     baseURL: apis,
+    baseURL: apis,
     withCredentials: true,
     headers: {
         "Content-Type": "multipart/form-data",
@@ -28,19 +28,21 @@ const config = {
 // login api 
 export const loginApi = (data) => Api.post('api/signin', data);
 export const registerApi = (data) => Api.post('/api/signup', data);
-export const profileApi = (id) => Api.post(`/api/profile/${id}`, );
+export const profileApi = (id) => Api.post(`/api/profile/${id}`,);
 
 //product
 export const createProductApi = (data) => ApiWithFormData.post('/api/products', data);
 export const getAllProductApi = () => Api.get('/api/products');
 export const getProductByIDApi = (id) => Api.get(`/api/products/${id}`);
+export const updateProductApi = (id, data) => ApiWithFormData.put(`/api/products/${id}`, data);
+export const deleteProductApi = (id, ) => Api.delete(`/api/products/${id}`);
 
 //cart
 export const addToCartApi = (data) => Api.post('/api/cart/add', data);
 export const getAllCartItems = (id) => Api.get(`/api/cart/get/${id}`);
 // Correct removeFromCart API to send data in axios.delete config
-export const removeFromCart = (data) => 
-  Api.delete('/api/cart/delete', { data, headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
+export const removeFromCart = (data) =>
+    Api.delete('/api/cart/delete', { data, headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
 
 
 
